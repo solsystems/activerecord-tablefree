1df4ce135f749b42ec629e5e741589d831009004
Allen Taylor <akt@frikshun.com>
2023-03-07 12:19:44 -0500

    WIP

--- lib/activerecord/tablefree.rb
+++ lib/activerecord/tablefree.rb
@@ -87,7 +87,7 @@ module ActiveRecord
 
       # Register a new column.
       def column(name, sql_type = nil, default = nil, null = true)
-        cast_type = ActiveRecord::Type.const_get(sql_type.to_s).new
+        cast_type = ActiveRecord::Type.const_get(sql_type.to_s.camelize).new
 
         sql_type_metadata = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(
           sql_type: cast_type.type.to_s,
